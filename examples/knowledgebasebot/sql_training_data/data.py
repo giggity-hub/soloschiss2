10e64204975cfe94e1cef81de20364da08256276
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-07-11 18:50:44 +0200

    added training data

--- examples/knowledgebasebot/sql_training_data/data.py
+++ examples/knowledgebasebot/sql_training_data/data.py
@@ -1,106 +1,137 @@
 from parametrize import parametrize, parametrize_all
+from to_soloist_format import to_soloist_format
+import json
 
-def d(user, belief, system):
-    return {
-        "user": user,
-        "system": system,
-        "belief": belief,
+ask_for_museum_about = parametrize({
+    "user": "%(user_input)s",
+    "belief": "df = domains['museum'].query('about == \"%(about)s\"') ; row = None ; names = df['name'].tolist()",
+    "system": "Here are some museums about %(about)s {names}",
+    }, 
+    {"user_input": [
+        "I would like to see a museum about %(about)s",
+        "I want to go to a museum about %(about)s",
+        "I'd like to visit a %(about)s museum",
+        "Please recommend some %(about)s museums for me",
+        "are there any good museums about %(about)s in Stuttgart",
+        "Show me a list of museums with the topic %(about)s",
+        "Recommend some %(about)s museums for me",
+        "What museums are in Stuttgart, that are about %(about)s",
+        "Do you know of any good %(about)s museums here?"]
     }
+)
+
+_ask_for_attribute_by_name = parametrize({
+    "user": "%(user)s",
+    "belief": "row = df.query('name == \"%(name)s\"') ; %(key)s = row['%(key)s']",
+    "system": "The %(attribute)s of the %(name)s is {%(key)s}"
+    },
+    {
+        "user": [
+            "What is the %(attribute)s of the %(name)s",
+            "Could you tell me the %(attribute)s of the %(name)s",
+            "Regarding the %(name)s what is its %(attribute)s",
+            "Would you be so kind to inform me about the %(attribute)s of the %(name)s",
+            "Do tell me what the %(attribute)s of the %(name)s is",
+            "Tell me the %(attribute)s of the %(name)s",
+            "What's the %(attribute)s of %(name)s",
+            "What is %(name)s's %(attribute)s",
+            "What is the of %(attribute)s of the %(name)s"
+        ]
+    }
+)
+
+ask_for_attribute_by_name = parametrize_all(
+    _ask_for_attribute_by_name,
+    {
+        "name, attribute, key": [
+            ("Oblomov", "address", "address"),
+            ("L'Osteria", "phone number", "phone_number"),
+            ("Gutenberg", "wine press", "wine_press"),
+            ("Ikea", "parking lot size", "parking_lot_size"),
+            ("Mc Donald's", "annual revenue", "annual_revenue"),
+            ("Titanic", "passenger count", "passenger_count"),
+            ("Steel", "hardness", "hardness"),
+            ("Sun", "diameter", "diameter"),
+            ("Willies Tower", "height", "height"),
+            ("garage sale", "date", "date")
+        ]
+    }, max_len=20
+)
+
+_ask_for_attribute_by_index = parametrize({
+    "user": "%(user)s",
+    "belief": "row = df.iloc[%(index_num)s] ; %(key)s = row['%(key)s'] ; name = row['name']",
+    "system": "The %(attribute)s of the {name} is {%(key)s}"
+    },
+    {
+        "user": [
+            "What is the %(attribute)s of the %(index_name)s one?",
+            "What's the %(attribute)s of the %(index_name)s?",
+            "Tell me the %(attribute)s of the %(index_name)s %(domain)s",
+            "Can you lookup the %(attribute)s of the %(index_name)s %(domain)s for me please?",
+            "I want to know the %(attribute)s of the %(index_name)s"
+        ]
+    }
+)
+
+ask_for_attribute_by_index = parametrize_all(
+    _ask_for_attribute_by_index,
+    {
+        "attribute, key, domain": [
+            ("duration", "duration", "movie"),
+            ("author", "author", "book"),
+            ("trainer", "trainer", "team"),
+            ("driver", "driver", "car"),
+            ("size", "size", "building"),
+            ("elevation", "elevation", "view")
+        ],
+        "index_text, index_num": [
+            ("first", 0),
+            ("second", 1),
+            ("third", 2),
+            ("fourth", 3),
+            ("fifth", 4),
+            ("last", -1),
+            ("second last", -2)
+        ]
+    }, max_len=20
+)
+
+ask_for_restaurant_with_cuisine = parametrize({
+    "user": "%(user_input)s",
+    "belief": "df = domains['restaurant'].query('cuisine == \"%(cuisine)s\"') ; names = df['name'].tolist()",
+    "system": "These are restaurants that serve %(cuisine)s food {names}",
+    },
+    {
+        "user_input": [
+            "I would like to eat some %(cuisine)s food",
+            "I could really go for some %(cuisine)s food right now",
+            "Do you know some places that serve %(cuisine)s food?"
+        ]
+    })
+
+ask_for_museum = parametrize({
+    "user" : "%(user)s",
+    "belief" : "df = domains['museum'] ; about = df['about'].unique()",
+    "system" : "I know museums about the following topics {about}"
+    },
+    {
+        "user": [
+            "I would like to go to a museum",
+            "What kind of museums do you know of?"
+        ]
+    })
+
 
 data = [
-    # Wants to visit a museum about something
-    parametrize
-
-    parametrize({
-        "user": "One about %(about)s please",
-        "belief": "df = df.query('about == \"%(about)s\"') ; names = df['name'].tolist()",
-        "system": "Here are some museums about %(about)s {names}"
-    }, {'about': ['history', 'art', 'war', 'tanks', 'music', 'culture', 'ethnology']}),
-    {
-        "user": "Which of those have a discount for students?",
-        "belief": "names = df.query('student_discount == True')",
-        "system": "These art museums all offer a discount for students {names}"
-    },
-    parametrize({
-        "user": "Very well. I would like to go to the %(name)s museum. What is the address?",
-        "belief": "row = df.query('name == \"%(name)s\"').iloc[0] ; address = row['address']",
-        "system": "You can find the Gugelhupf museum at {address}"
-    }, {"name": ['Gugelhupf', 'Staatsgallerie', 'Heimatland', 'Koerperwelten']}),
-
-    {
-        "user" : "I would like to visit a history museum",
-        "belief" : "df = domains['museum'].query('about == \"history\"') ; names = df['name'].tolist()",
-        "system" : "These are museums about history {names}"
-    },
-        {
-            "user": "Nevermind, i'd rather go to a museum about tanks",
-            "belief": "df = domains['museum'].query('about == \"tanks\"') ; names = df['name'].tolist()",
-            "system": "Here are museums about tanks {names}"
-        },
-        {
-            "user": "What are the opening hours of the Panzerland?",
-            "belief": "row = df.query('name == \"Panzerland\"').iloc[0] ; opening_hours = row['opening_hours']",
-            "system": "The opening hours of the Panzerland are {opening_hours}"
-        },
-        {
-            "user": "What is the address?",
-            "belief": "address = row['address'] ; name = row['name']",
-            "system": "The address of the {name} is {address}"
-        },
-        {
-            "user": "What are the url and the phone number of the third one?",
-            "belief": "row = df.iloc[4] ; name = row['name'] ; url = row['url'] ; phone_number = row['phone_number']",
-            "system": "The url of the {name} is {url} and the phone number is {phone_number}"
-        }
-    ],
-    [
-        {
-            "user": "Recommend some restaurants for me",
-            "belief": "df = domains['restaurant'] ; cuisines = df['cuisine'].unique()",
-            "system": "Which type of food do you like? Choose one of these cuisines {cuisines}"
-        },
-        {
-            "user": "I want to eat chinese today",
-            "belief": "df = domains['restaurant'].query('cuisine == \"chinese\"') ; names = df['name'].tolist()",
-            "system": "Here are some chinese restaurants you might like {names}"
-        },
-        {
-            "user": "The Yellow River sounds nice. Where can i find it?",
-            "belief": "row = df.query('name == \"Yellow River\"') ; address = row['address']",
-            "system": "You can find the Yellow River at {address}"
-        },
-        {
-            "user": "And the second one, what is the address of that place?",
-            "belief": "row = df.iloc[1] ; name = row['name'] ; address = row['address']",
-            "system": "The address of the {name} is {address}"
-        }
-    ],
-    [
-        {
-            "user": "Hello. What can you talk about?",
-            "belief": "domain_keys = domains.keys()",
-            "system": "I can talk to you about places in Stuttgart like {domain_keys}"
-        },
-        {
-            "user": "I would like to go to a restaurant",
-            "belief": "df = domains['restaurant'] ; cuisines = df['cuisine'].unique()",
-            "system": "What kind of cuisine are you looking for? I know of the following {cuisines}"
-        },
-        {
-            "user": "I want to eat mexican food",
-            "belief": "df = domains['restaurant'].query('cuisine == \"mexican\"') ; names = df['name'].tolist()",
-            "system": "Here are some mexican restaurants i know of {names}"
-        },
-        {
-            "user": "What is the address of the first one?",
-            "belief": "row = df.iloc[0] ; address = row['address'] ; name = row['name']",
-            "system": "The address of the {name} is {address}"
-        },
-        {
-            "user": "Does the Salitos have free wifi?",
-            "belief": "row = df.query('name == \"Salitos\"').iloc[0] ; free_wifi = row['free_wifi']",
-            "system": "The value of 'free_wifi' for the Salitos is {free_wifi}"
-        }
-    ]
-
+    *parametrize_all(ask_for_museum_about, {
+        "about": ['art', 'history', 'war', 'cars']}, max_len=20),
+    *parametrize_all(ask_for_restaurant_with_cuisine, {
+        "cuisine": ["mexican", "indian", "german", "swedish", "chinese", "american", "italian"]}, max_len=20),
+    *ask_for_attribute_by_name,
+    *ask_for_attribute_by_index
 ]
+
+with open('data.json', 'w+') as f:
+    data = list(map(to_soloist_format, data))
+    json.dump(data, f, indent=4)
