7f9235eb3293a995bcc007a3f023d01c88b23e44
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-07-17 01:41:08 +0200

    update training_epochs and max_seq

--- soloist/scripts/train_kbbot.sh
+++ soloist/scripts/train_kbbot.sh
@@ -6,11 +6,11 @@ CUDA_VISIBLE_DEVICES=0 python3 soloist_train.py \
 --do_train \
 --train_data_file=data/$1 \
 --per_gpu_train_batch_size 1 \
---num_train_epochs 1 \
+--num_train_epochs 10 \
 --learning_rate 5e-5 \
 --overwrite_cache \
 --save_steps 10000 \
---max_seq 10 \
+--max_seq 100 \
 --overwrite_output_dir \
 --max_turn 1 \
 --mc_loss_efficient 1 
