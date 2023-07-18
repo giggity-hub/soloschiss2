fad3bfc1410e60a81ec5ec8321d98ae8c75b5c73
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-07-18 10:30:51 +0200

    renamed some stuff for clarification

--- domains/stairs/scripts.py
+++ domains/stairs/scripts.py
@@ -1,13 +1,13 @@
 import os 
 import pandas as pd
 
-def main():
+def clean():
     this_dir = os.path.dirname(__file__)
     table_path = os.path.join(this_dir, 'raw.csv')
     df = pd.read_csv(table_path)
     columns = ['name', 'area', 'number_of_steps', 'length', 'height', 'named_after']
     
-    # reduce the number of columns
+    # drop all undesired columns
     df = df[columns]
     # drop rows with missing values
     df = df.dropna()
@@ -16,4 +16,4 @@ def main():
     df.to_csv(out_path, index=False)
 
 if __name__ == "__main__":
-    main()
+    clean()
