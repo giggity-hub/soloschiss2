a03921d9d1801edeb0976596299c4152dd8b0b82
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-07-19 19:53:22 +0200

    setup for fill_template_slots

--- stuttbard/chatbot/evaluate.py
+++ stuttbard/chatbot/evaluate.py
@@ -1,3 +1,5 @@
+import re
+
 def parse_beliefstate(beliefstate_string: str):
     if '=' not in beliefstate_string:
         return {}
@@ -37,6 +39,30 @@ def resolve_entity(bs, df, entity):
     return entity
 
 
+
+def render_entity_value():
+    pass
+
+def render_column():
+    pass
+
+def find_all_slots_in_template(template_string):
+    # define this pattern
+    pattern = ''
+    matches = [m for m in re.finditer(pattern, template_string)]
+    return matches
+
+def fill_template_slots(template_string, df, entity):
+    matches = find_all_slots_in_template()
+    pass
+    # 1.) find all substrings of the form slot_df_xxxx or slot_entity_xxx
+    # column names may also include underscores but never a space
+    # slot_df_xxx means that column xxx should be displayed
+    # slot_entity_xxx means that attribute xxx of the entity should be displayed
+    # 2.) Once you have all the matches replace the slots in the string with correct values
+    # for columns (slot_df_xxx) use render_column() and for entity values (slot_entity_xxx) use render_entity_value()
+
+
 def evaluate(sample, domains, df, entity):
     beliefstate_string = sample['belief']
     bs = parse_beliefstate(beliefstate_string)
@@ -44,9 +70,10 @@ def evaluate(sample, domains, df, entity):
     entity = resolve_entity(bs, df, entity)
 
     response_template = sample['system']
-    res = eval('f"' + response_template.strip() + '"')
+    res = fill_template_slots(response_template, df, entity)
     return res
-    return res
+
+
 
 
 
--- tests/test_chat.py
+++ tests/test_chat.py
@@ -1,6 +1,8 @@
-from stuttbard.chatbot.evaluate import parse_beliefstate, create_df, evaluate, resolve_entity
+from stuttbard.chatbot.evaluate import parse_beliefstate, create_df, evaluate, resolve_entity, find_all_slots_in_template
 import pytest
 import pandas as pd
+from collections import namedtuple
+import re
 
 @pytest.mark.parametrize("test_input, expected", [
     ("hello = World ; myval = 2", {'hello': 'World', 'myval': '2'}),
@@ -29,10 +31,29 @@ def test_resolve_entity(domains):
     assert entity['name'] == 'Little Ceasars'
 
 
-def test_evaluate(domains):
-    sample = {
-        'belief': "domain = restaurant ; query = cuisine == 'italian' ; entity_index = 0",
-        'system': "The name of the place is {entity['name']}"
-    }
-    res = evaluate(sample, domains, None, None)
-    assert res == "The name of the place is Little Ceasars"
+Match = namedtuple("Match", "start stop group")
+
+@pytest.mark.parametrize("test_input, expected", [
+    ("slot_df_name", [Match(0, 12, 'slot_df_name')]),
+    ('xxx hhh iii mmm slot_df_name sdfasd', [Match(16, 28, 'slot_df_name')]),
+    ('slot_entity_phone_number', [Match(0, 24, 'slot_entity_phone_number')]),
+    ('slot_entity_phone number', [Match(0, 17, 'slot_entity_phone')]),
+    ('xxx slot_entity_a slot_entity_b xxx', [Match(4,17, 'slot_entity_a'), Match(18, 31, 'slot_entity_b')])
+])
+def test_find_all_slots_in_template(test_input, expected):
+    matches = find_all_slots_in_template(test_input)
+    assert len(matches) == len(expected)
+
+    for match, match_expected in zip(matches, expected):
+        assert match.start() == match_expected.start
+        assert match.end() == match_expected.end
+        assert match.group() == match_expected.group
+
+
+# def test_evaluate(domains):
+#     sample = {
+#         'belief': "domain = restaurant ; query = cuisine == 'italian' ; entity_index = 0",
+#         'system': "The name of the place is {entity['name']}"
+#     }
+#     res = evaluate(sample, domains, None, None)
+#     assert res == "The name of the place is Little Ceasars"
