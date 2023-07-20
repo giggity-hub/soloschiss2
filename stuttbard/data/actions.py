bb844c75f4d126f01a746e091b6038e7f32403dc
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-07-20 20:37:51 +0200

    use simplified syntax

--- stuttbard/data/actions.py
+++ stuttbard/data/actions.py
@@ -1,7 +1,8 @@
 def main(domain_sampler, parametrize):
+    return []
 
     call_by_mention = parametrize({
-        "belief": "phone_number = row['phone_number'] ; actions.call(phone_number)",
+        "belief": "action = call",
         "user_system": [
             ("Call them", "Calling {phone_number} ..."),
             ("Please call their telephone number", "Calling {phone_number} ..."),
@@ -10,7 +11,7 @@ def main(domain_sampler, parametrize):
     })
 
     call_by_name = parametrize({
-        "belief": "phone_number = row['phone_number'] ; actions.call(phone_number)",
+        "belief": "action = call",
         "samplers": {"name": domain_sampler['name']},
         "user_system": [
             ("Call the phone number of the %(name)s please", "Calling {phone_number} ..."),
@@ -19,20 +20,20 @@ def main(domain_sampler, parametrize):
         ]
     })
 
-    call_by_index_and_domain = parametrize({
-        "belief": "row = df.iloc[%(index_int)s] ; phone_number = row['phone_number'] ; actions.call(phone_number)",
-        "samplers": {
-            "index_str, index_int": domain_sampler['index'],
-            "domain": domain_sampler["keys"]},
-        "user_system": [
-            ("I want to make a call to the %(index_str)s %(domain)s", "Calling {phone_number} ..."),
-            ("Connect me with the %(index_str)s via phone", "Calling {phone_number} ..."),
-            ("Call the %(index_str)s %(domain)s", "Calling {phone_number} ...")
-        ]
-    })
+    # call_by_index_and_domain = parametrize({
+    #     "belief": "row = df.iloc[%(index_int)s] ; phone_number = row['phone_number'] ; actions.call(phone_number)",
+    #     "samplers": {
+    #         "index_str, index_int": domain_sampler['index'],
+    #         "domain": domain_sampler["keys"]},
+    #     "user_system": [
+    #         ("I want to make a call to the %(index_str)s %(domain)s", "Calling {phone_number} ..."),
+    #         ("Connect me with the %(index_str)s via phone", "Calling {phone_number} ..."),
+    #         ("Call the %(index_str)s %(domain)s", "Calling {phone_number} ...")
+    #     ]
+    # })
 
     open_website_by_mention = parametrize({
-        "belief": "url = row['website'] ; actions.open_website(url)",
+        "belief": "action = open_website",
         "user_system": [
             ("Can you show me the website?", "Opening {url} in the browser ..."),
             ("Take me to their url", "Opening {url} in the browser ..."),
