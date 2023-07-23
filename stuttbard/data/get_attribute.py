7c04c888751995a90b0e891d85269e6c30a2d44b
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-07-23 17:06:08 +0200

    use entity sampler to get attributes

--- stuttbard/data/get_attribute.py
+++ stuttbard/data/get_attribute.py
@@ -2,114 +2,68 @@ from utils.unique_random import UniqueRandom
 #from domains.domains import load_domains_dict, load_domain_sampler
 
 def main(domain_sampler, parametrize):
-    attributes_and_col_name = [
-        ('number of steps', 'number_of_steps'),
-        ('height')
-    ]
 
+    # Add some more user_system examples here
+    ask_for_single_attr_without_mention = parametrize({
+        'belief': '',
+        'user_system': [
+            ('Give me the address of that place %(entity_mention)s',
+                'The address of the slot_entity_name is slot_entity_address'),
+            ('Where is it located?',
+                'The slot_entity_name is located at slot_entity_address')
+        ]
+    })
 
-    # Include 1 question for every column name in our datasets
-    # Also include attributes which are not in the dataset. hopefully this will lead to better generalization
-    # DO NOT INCLUDE SYNONYMS: topic should not be recognized as the about column and instead as the topic column
-    # We can resolve synonyms in the chatbot. this makes it easier for the model
-    user_system = [
-        ('What is the number of steps of the %(entity_mention)s', 
+    # Add some more user_system examples here
+    ask_for_single_attr_by_mention = parametrize({
+        'belief': '%(mention_belief_state)s',
+        'samplers': {
+            'entity_mention, mention_belief_state': domain_sampler['entity']
+        },
+        'user_system': [
+        ('What is the name of the %(entity_mention)s?',
+            'The name of the slot_entity_name is slot_entity_name'),
+        ('I want to know which area the %(entity_mention)s is in',
+            'The slot_entity_name is in the slot_entity_area area'),
+        ('What is the number of steps of the %(entity_mention)s?', 
             'The slot_entity_name has slot_entity_number_of_steps steps'),
-        ('How tall is the %(entity_mention)s', 
+        ('How many steps does the %(entity_mention)s have?',
+            'The slot_entity_name has slot_entity_number_of_steps steps'),
+        ('Tell me the length of the %(entity_mention)s',
+            'The length of the slot_entity_name is slot_entity_length'),
+        
+        ('What is the price of the %(entity_mention)s',
+            'The price of the slot_entity_name is slot_entity_price'),
+        ('I demand that you tell me the purpose of the %(entity_mention)s',
+            'The purpose of the slot_entity_name is slot_entity_purpose'),
+        ('Can you find out what the annual revenue of the %(entity_mention)s is please?',
+            'The annual revenue of the slot_entity_name is slot_entity_annual_revenue'),
+        ('Look up the halflife time of the %(entity_mention)s',
+            'The halflife time of the slot_entity_name is slot_entity_half_life_time'),
+        ('Find out what the sugar content of the %(entity_mention)s is for me',
+            'The sugar content of the slot_entity_name is slot_entity_sugar_content'),
+        
+        ('How tall is the %(entity_mention)s?', 
             'The slot_entity_name is slot_entity_tall tall'),
-        ('What is the rating of the %(entity_mention)s',
+        ('What is the rating of the %(entity_mention)s?',
             'The rating of the slot_entity_name is slot_entity_rating'),
         ('Please give me the phone number of the %(entity_mention)s',
             'The phone number of the slot_entity_name is slot_entity_phone_number'),
         ('Could you provide me with the url of the %(entity_mention)s',
-            'The url of the slot_entity_name is slot_entity_url')
+            'The url of the slot_entity_name is slot_entity_url'),
+        ('Do you know the size of the %(entity_mention)s?',
+            'The size of the slot_entity_name is slot_entity_size'),
+        ('What is the maximum occupancy of the %(entity_mention)s?',
+            'The maximum occupancy of the slot_entity_name is slot_entity_maximum_occupancy'),
+        ('What is the loudness of the %(entity_mention)s?',
+            'The loudness of the slot_entity_name is slot_entity_loudness'),
+        ('Can you please tell me the address of the %(entity_mention)s',
+            'The address of the slot_entity_name is slot_entity_address'),
+        ('And what is the duration of the %(entity_mention)s?',
+            'The duration of the slot_entity_name is slot_entity_duration'),
     ]
-
-    # Extend the following list of entity mentions
+    })
     
-    # Der domain_sampler hat eine funktion um einen random index zu bekommen
-    # domain_sampler['index'].sample() gibt z.b. ('first', 1)
-    # 1.) fuege zu dem domain sampler eine sample methode hinzu sodass du eine zufaellige domain bekommnst
-    # 2.) fuege zu dem domain sampler eine sample methode hinzu sodass du einen zufaelligen namen bekommst
-    # Den domain sampler findest du in domains.domains
-
-    # Mentions can be of five types (unless i forgot some)
-    # generate mentions of every type. Utilize the domain sampler and the index, domain and name sample methods
-    entity_mentions_bs_and_type = [
-        # type 1: just the name
-        ('Hotalo', 'Hotalo', 'name'),
-        # type two: index + 'one'
-        ('first one', 0, 'index'),
-        # type three: just index
-        ('last', -1, 'index'),
-        # type four: index + domain name
-        ('first restaurant', 0, 'index'),
-        # type five name + domain name
-        ('StadtPalais museum', 'StadtPalais', 'name')
-    ]
-    # Below are two functions which generate mentions of a given type
-    def generate_type_five(n): # not sure if it works
-        res = []
-        for i in range(n):
-            # assuming that you have defined a "name" sampler in domains.domains
-            rand_name = domain_sampler['name'].sample()
-            rand_domain = domain_sampler['domain'].sample()
-            res.append((f'{rand_name} {rand_domain}', rand_name, 'name'))
-        return res
-
-    def generate_type_one(n): # works
-        res = []
-        for i in range(n):
-            rand_name = domain_sampler['name'].sample()
-            res.append((rand_name, rand_name, 'name'))
-        return res
-
-    def generate_type_two(n): # works
-        res = []
-        for i in range(n):
-            index_str, index_int = domain_sampler['index'].sample()
-            res.append((index_str + ' one', index_int, 'index'))
-        return res
-    
-    def generate_type_three(n): #works
-        res = []
-        for i in range(n):
-            index_str, index_int = domain_sampler['index'].sample()
-            res.append((index_str, index_int, 'index'))
-        return res
-
-    def generate_type_four(n): #works
-        res = []
-        for i in range(n):
-            index_str, index_int = domain_sampler['index'].sample()
-            rand_domain = domain_sampler['domain'].sample()
-            res.append((index_str + " " + rand_domain, index_int, 'index'))
-        return res
-
-    
-    
-    # Now you also need to write a function which constructs a belief state given a 
-    res = []
 
 
-
-    """
-    ################
-    no_examples = 3
-    response1 = generate_type_one(no_examples)
-    response2 = generate_type_two(no_examples)
-    response3 = generate_type_three(no_examples)
-    response4 = generate_type_four(no_examples)
-    response5 = generate_type_five(no_examples)
-    print("response1:", response1)
-    print("response2:", response2)
-    print("response3:", response3)
-    print("response4:", response4)
-    print("response5:", response5)
-    """
-
-"""
-domains_dict = load_domains_dict()
-domain_sampler = load_domain_sampler(domains_dict=domains_dict)
-main(domain_sampler=domain_sampler, parametrize=None)
-"""
+    return [*ask_for_single_attr_by_mention]
