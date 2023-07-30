5765defe95236d1ecddbaad3339348dca7dff427
giggity-hub <71182329+giggity-hub@users.noreply.github.com>
2023-07-30 20:41:12 +0200

    shitty script

--- utils/shit.py
+++ utils/shit.py
@@ -6,21 +6,21 @@ def unroll_belief_state(moped):
     return res
 
 moped = {
-        "belief": "domain = restaurants ; head = 3 ; sortby = rating; cuisine = %(cuisine)s",
-        "samplers": {"cuisine": lambda s: s['restaurant']['cuisine']},
+        'samplers': {'index_text, index_int': lambda s : s['index']},
+        'belief': "domain = museum; entity_index = %(index_int)s",
         "user_system": [
-            ("What are the best %(cuisine)s restaurants in town?",
-                "The three highest-rated %(cuisine)s restaurants are: slot_df_name"),
-            ("What are the top 3 restaurants that serve %(cuisine)s food",
-                "Here are the three highest-rated %(cuisine)s restaurants: slot_df_name"),
-            ("Give me the top-3 %(cuisine)s restaurants!",
-                "The three highest-rated %(cuisine)s restaurants are: slot_df_name"),
-            ("Which %(cuisine)s restaurants have the highest rating?",
-                "Here are the three highest rated restaurants: slot_df_name"),
-            ("Do you know which %(cuisine)s restaurants are highest rated?",
-             "Here are the three highest-rated %(cuisine)s restaurants: slot_df_name"),
-            ("What is the best restaurant that serves %(cuisine)s cuisine?",
-                "Here are the three highest-rated %(cuisine)s restaurants: slot_df_name"),
+            ("Does the %(index_text)s museum has an accessible entrance for wheelchairs?", "The entrance of the slot_entity_name museum is for wheelchairs: slot_entity_wheelchair_accessible_entrance."),
+            ("What is the adress of the %(index_text)s museum?", "The adress of the slot_entity_name museum is slot_entity_vicinity."),
+            ("Can you give me the link to the website of the %(index_text)s museum?", "The link to the %(index_text)s museum is slot_entity_website."),
+            ("Give me the house number of the %(index_text)s museum!", "The slot_entity_name museum has the house number slot_entity_house_number."),
+            ("Im interested in the %(index_text)s museum. Show me the phone number!", "slot_entity_phone_number is the phone number of the slot_entity_name museum."),
+            ("Give me the phone number of the %(index_text)s one!", "The slot_entity_name museum can be reached under slot_entity_phone_number."),
+            ("In which street is the %(index_text)s one", "The slot_entity_name museum sits in the slot_entity_street street."),
+            ("I want to know the rating of the %(index_text)s one", "The %(index_text)s museum has an average rating of slot_entity_rating on google maps."),            
+            ("How popular is the %(index_text)s museum?", "The average rating of the %(index_text)s museum is slot_entity_rating."),
+            ("Could you give me the google plus code of the %(index_text)s one?", "Sure, the google plus code of the slot_entity_name museum is slot_entity_plus_code."),
+            ("What is the topic of the %(index_text)s museum?", "The %(index_text)s museum is about slot_entity_about or more specific about slot_entity_about_specific."),
+            ("I would like to visit the %(index_text)s museum. In which area is it?", "The slot_entity_name museum is in the slot_entity_area.")
         ]
     }
 
